483bb5861ce72feff39b9c08c5e5490f2dd1e51a
Philipp Uhl <git@ph-uhl.com>
2019-01-22 17:16:26 +0100

    Fixes db-type check

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function(c, next){
-  if(c.use && c.use in ['postgresql']){
+  if(c.use && ['postgresql'].indexOf(c.use) !== -1){
     require('./' + c.use).connect(c[c.use], next);
   } else {
     throw "Config references DB-engine that does not exist: " + c.use;
